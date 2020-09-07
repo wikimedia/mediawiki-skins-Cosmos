c7a14b9b5025a80e0cffef83440f864174d166ae
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-09-07 10:51:48 -0500

    Remove deprecated lightmode

--- includes/CosmosHooks.php
+++ includes/CosmosHooks.php
@@ -1,15 +1,6 @@
 <?php
 class CosmosHooks extends Hooks {
 	public static function onGetPreferences( $user, &$preferences ) {
-		$preferences['cosmos-colorscheme'] = [
-			'type' => 'radio',
-			'label-message' => 'cosmos-colorscheme-preference',
-			'section' => 'cosmos/cosmos-colorscheme',
-			'options' => [
-				wfMessage( 'cosmos-darkmode-preference' )->text() => 'cosmos-darkmode',
-				wfMessage( 'cosmos-lightmode-preference' )->text() => 'cosmos-lightmode',
-			],
-		];
 		$preferences['cosmos-mobile-responsiveness'] = [
 			'type' => 'check',
 			'label-message' => 'cosmos-mobile-preference',
