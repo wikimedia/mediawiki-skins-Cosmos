28efa0673685104070928bebc1176e3fad4d118e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-09-17 11:38:07 -0500

    format

--- includes/SkinCosmos.php
+++ includes/SkinCosmos.php
@@ -18,78 +18,84 @@ class SkinCosmos extends SkinTemplate {
 	/**
 	 * @param OutputPage $out
 	 */
-	public function initPage( OutputPage $out ) {
-		parent::initPage( $out );
+	public function initPage(OutputPage $out) {
+		parent::initPage($out);
 		$config = new Config();
-		if($this->getSkin()->getUser()->getOption( 'cosmos-mobile-responsiveness') == 1){
-	       		 $out->addMeta( 'viewport',
-				'width=device-width, initial-scale=1.0, ' .
+		if ($this->getSkin()
+			->getUser()
+			->getOption('cosmos-mobile-responsiveness') == 1) {
+			$out->addMeta(
+				'viewport', 
+				'width=device-width, initial-scale=1.0, ' . 
 				'user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0'
 			);
 		}
-		$out->addModuleStyles( [
-			'mediawiki.skinning.content.externallinks',
-			'skins.cosmos',
-			'skins.cosmos.legacy',
-		] );
-		
-		$out->addModules( [
-			'skins.cosmos.js',
+		$out->addModuleStyles([
+			'mediawiki.skinning.content.externallinks', 
+			'skins.cosmos', 
+			'skins.cosmos.legacy'
+		]);
+
+		$out->addModules([
+			'skins.cosmos.js', 
 			'skins.cosmos.mobile'
-		] );
-	if (ExtensionRegistry::getInstance()->isLoaded( 'Portable Infobox' )){
-		 $out->addModuleStyles( [
-			        'skins.cosmos.portableinfobox',
-		        ] );
-		if($config->isEnabled( 'europa-theme' )){
-			 $out->addModuleStyles( [
-			        'skins.cosmos.portableinfobox.europa',
-		        ] );
-		}else {
-			 $out->addModuleStyles( [
-			        'skins.cosmos.portableinfobox.default',
-		        ] );
+		]);
+		if (ExtensionRegistry::getInstance()
+			->isLoaded('Portable Infobox')) {
+			$out->addModuleStyles([
+				'skins.cosmos.portableinfobox', 
+			]);
+			if ($config->isEnabled('europa-theme')) {
+				$out->addModuleStyles([
+					'skins.cosmos.portableinfobox.europa', 
+				]);
+			}
+			else {
+				$out->addModuleStyles([
+					'skins.cosmos.portableinfobox.default', 
+				]);
+			}
+		}
+		//Load SocialProfile styles if the respective configuration variables are enabled
+		if (class_exists('UserProfilePage')) {
+			if ($config->isEnabled('modern-tabs')) {
+				$out->addModuleStyles([
+					'skins.cosmos.profiletabs', 
+				]);
+			}
+			if ($config->isEnabled('round-avatar')) {
+				$out->addModuleStyles([
+					'skins.cosmos.profileavatar', 
+				]);
+			}
+			if ($config->isEnabled('show-editcount')) {
+				$out->addModuleStyles([
+					'skins.cosmos.profileeditcount', 
+				]);
+			}
+			if ($config->isEnabled('allow-bio')) {
+				$out->addModuleStyles([
+					'skins.cosmos.profilebio', 
+				]);
+			}
+			if ($config->isEnabled('profile-tags')) {
+				$out->addModuleStyles([
+					'skins.cosmos.profiletags', 
+				]);
+			}
+			if ($config->isEnabled('modern-tabs') || 
+			    $config->isEnabled('round-avatar') || 
+			    $config->isEnabled('show-editcount') || 
+			    $config->isEnabled('allow-bio') || 
+			    $config->isEnabled('profile-tags')) {
+				$out->addModuleStyles([
+					'skins.cosmos.socialprofile', 
+				]);
+			}
+		}
+		if ($out->getTitle()
+			->equals(Title::newMainPage())) {
+			$out->addBodyClasses('mainpage');
 		}
 	}
-		//Load SocialProfile styles if the respective configuration variables are enabled
-        if (class_exists('UserProfilePage')){
-            if($config->isEnabled( 'modern-tabs' )){
-                $out->addModuleStyles( [
-			        'skins.cosmos.profiletabs',
-		        ] );
-            }
-            if($config->isEnabled( 'round-avatar' )){
-                $out->addModuleStyles( [
-			        'skins.cosmos.profileavatar',
-		        ] );
-            }
-           if($config->isEnabled( 'show-editcount' )){
-                $out->addModuleStyles( [
-			        'skins.cosmos.profileeditcount',
-		        ] );
-            }
-           if($config->isEnabled( 'allow-bio' )){
-                $out->addModuleStyles( [
-			        'skins.cosmos.profilebio',
-		        ] );
-            }
-           if($config->isEnabled( 'profile-tags' )){
-                $out->addModuleStyles( [
-			        'skins.cosmos.profiletags',
-		        ] );
-            }
-           if($config->isEnabled('modern-tabs') ||
-		   $config->isEnabled('round-avatar') ||
-		   $config->isEnabled('show-editcount') || 
-		   $config->isEnabled('allow-bio') ||
-		   $config->isEnabled('profile-tags')){
-		       $out->addModuleStyles( [
-			        'skins.cosmos.socialprofile',
-		        ] );
-		   }
-     }
-     if($out->getTitle()->equals(Title::newMainPage()) ){
-        $out->addBodyClasses('mainpage');
-     }
-  }
 }
