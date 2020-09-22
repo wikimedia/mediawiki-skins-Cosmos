192ba340541836a520d917a4e1110e52c21688ce
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-09-22 15:15:43 -0500

    add new hook

--- includes/CosmosHooks.php
+++ includes/CosmosHooks.php
@@ -7,5 +7,17 @@ class CosmosHooks extends Hooks {
 			'section' => 'cosmos/cosmos-responsiveness'
 		];
 	}
+	public static function onOutputPageBodyAttributes( OutputPage $out, Skin $skin, &$bodyAttrs ) {
+		if ( $skin->getUser()->isLoggedIn() ) {
+			$bodyAttrs['class'] .= ' user-logged';
+		}
+		else {
+			$bodyAttrs['class'] .= ' user-anon';
+		}
+		if ($out->getTitle()->equals(Title::newMainPage())) {
+			$bodyAttrs['class'] .= ' mainpage';
+		}
+		return true;
+	}
 }
 
