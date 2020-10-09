3d9050b6c57c609e9e7fe1b6126762bdc9fbbef1
Sam Nguyen <sam.t.nguyenn@gmail.com>
2020-10-09 13:10:48 -0500

    Setup Grunt, ESLint, and Stylelint
    Setup the three main standard development packages for Cosmos.
    All rules that the JS/CSS/LESS code currently violates, are
    currently turned off. In future patches, groups of related rules
    will be gradually enabled. The goal is that in the future,
    every rule will be enabled by default.
    
    Bug: T265104
    Change-Id: Ibe668d14b87e0c94f87424064e9990ef08f21396

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/* eslint-env node */
+
+module.exports = function ( grunt ) {
+	var config = grunt.file.readJSON( 'skin.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		banana: config.MessagesDirs,
+		eslint: {
+			all: [
+				'*.js',
+				'**/*.js',
+				'!node_modules/**',
+				'!vendor/**'
+			],
+			fix: {
+				options: {
+					fix: true
+				}
+			}
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'**/*.less',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'banana', 'eslint', 'stylelint' ] );
+	grunt.registerTask( 'fix', 'eslint:fix' );
+	grunt.registerTask( 'default', 'test' );
+};
