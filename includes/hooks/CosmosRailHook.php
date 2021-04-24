a5cc26fe379512f6867ee3353780ee9fbe8b626c
Universal Omega <1010delta1010@gmail.com>
2021-04-24 21:03:14 +0000

    [Cosmos] create interface for CosmosRail hook
    Change-Id: I87249d28682cb4497fd78bdb45660a5f0dbbbe2c

---
+++ includes/hooks/CosmosRailHook.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MediaWiki\Skin\Cosmos\Hook;
+
+use MediaWiki\Skin\Cosmos\CosmosRail;
+
+interface CosmosRailHook {
+	/**
+	 * @param CosmosRail $cosmosRail
+	 */
+	public function onCosmosRail( CosmosRail $cosmosRail ) : void;
+}
